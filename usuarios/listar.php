261fa6667c9794e735389a7a5a37b502e729c8e2
Camilly-Pires <camillyoliveirapires04@gmail.com>
2026-03-03 17:50:16 -0300

    dinamic table for queries

--- usuarios/listar.php
+++ usuarios/listar.php
@@ -1,6 +1,13 @@
 <?php
 require_once __DIR__ . '/../config.php';
+require_once BASE_PATH . '/src/usuario_crud.php';
 
+$usuario = buscarUsuario($conexao);
+
+/*
+echo "<pre>";
+var_dump($usuario);
+echo "</pre>";*/
 
 $titulo = "Usuários |";
 require_once BASE_PATH . '/includes/cabecalho.php';
@@ -9,7 +16,7 @@ require_once BASE_PATH . '/includes/cabecalho.php';
 <section class="text-center mb-4 border rounded-3 p-4 border-primary-subtle">
     <h3><i class="bi bi-person-gear"></i> Gerenciar Usuários</h3>
 
-    
+
 
     <p class="text-center my-4">
         <a href="inserir.php" class="btn btn-primary"><i class="bi bi-plus-circle"></i> Adicionar Novo Usuário</a>
@@ -27,11 +34,13 @@ require_once BASE_PATH . '/includes/cabecalho.php';
                 </tr>
             </thead>
             <tbody>
-                
+
+                <?php foreach ($usuario as $usuario) : ?>
+
                     <tr>
-                        <td>ID do Usuário...</td>
-                        <td>Nome do Usuário...</td>
-                        <td>E-mail do Usuário...</td>
+                        <td><?= $usuario['id'] ?></td>
+                        <td><?= $usuario['nome'] ?></td>
+                        <td><?= $usuario['email'] ?></td>
                         <td class="text-end">
                             <a class="btn btn-warning btn-sm" href="editar.php"><i class="bi bi-pencil-square"></i> Editar</a>
                         </td>
@@ -39,7 +48,12 @@ require_once BASE_PATH . '/includes/cabecalho.php';
                             <a class="btn btn-danger btn-sm" href="excluir.php"><i class="bi bi-trash"></i> Excluir</a>
                         </td>
                     </tr>
-                
+
+                <?php endforeach ?>
+
+
+
+
             </tbody>
         </table>
     </div>
